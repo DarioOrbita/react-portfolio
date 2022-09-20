9b65e25d6583413dc2912a2737cca79f5576a755
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-20 16:05:46 -0500

    add initial non-component links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 
 function Portfolio() {
-    return(
-        <section>
-            <h1 id="portfolio">Portfolio</h1>
-            <div>
-                <p>
-                    lorem ipsum. 
-                </p>
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <h1 id="portfolio">Portfolio</h1>
+      <div>
+        <li>
+          <a href="https://github.com/DarioOrbita/Movie-Finder">Movie Finder</a>
+        </li>
+        <li>
+          <a href="https://github.com/DarioOrbita/text-editor">
+            Just Another Text editor
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/DarioOrbita/hold-me-accountable">
+            Hold Me Accountable
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/DarioOrbita/taskmaster-pro">
+            Taskmaster Pro
+          </a>
+        </li>
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
