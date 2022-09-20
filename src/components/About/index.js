8943d33366e5de9a2d4edb03126b8c4ebeec6c34
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-20 15:54:24 -0500

    add about me img

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,9 +4,10 @@ function About() {
     return(
         <section>
             <h1 id="about">About Me</h1>
+            <img src={require('../../assets/panik.png')} alt="panik"></img>
             <div>
                 <p>
-                    lorem ipsum. 
+                    yadda yadda lorem ipsum i'm on a deadline
                 </p>
             </div>
         </section>
