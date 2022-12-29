102f3eb9113c2165983618f21cd156499d83b817
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-12-28 18:46:44 -0600

    change About Me image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,10 +4,10 @@ function About() {
     return(
         <section>
             <h1 id="about">About Me</h1>
-            <img src={require('../../assets/panik.png')} alt="panik"></img>
+            <img src={require('../../assets.IMG_20210110_145207_875.jpg')} alt="profile image"></img>
             <div>
                 <p>
-                    An avid problem solver that enjoys solving interesting problems and challenges. I grew up in Saint Paul, Minnesota 
+                    An avid problem solver that enjoys solving interesting problems and challenges. I grew up in Saint Paul, Minnesota. 
                 </p>
             </div>
         </section>
