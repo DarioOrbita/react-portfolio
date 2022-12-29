4b4a7d0cace23ed01f8a6fc2820161b708fdbed5
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-12-28 19:16:28 -0600

    fix img alt and rebuild

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
     return(
         <section>
             <h1 id="about">About Me</h1>
-            <img src={require('../../assets.IMG_20210110_145207_875.jpg')} alt="profile image"></img>
+            <img src={require('../../assets/IMG_20210110_145207_875.jpg')} alt="dario headshot"></img>
             <div>
                 <p>
                     An avid problem solver that enjoys solving interesting problems and challenges. I grew up in Saint Paul, Minnesota. 
